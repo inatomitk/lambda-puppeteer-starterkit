29b7f6e23cb5e3051598c3c933fef1dfaa1d43aa
inatomitk <35132611+inatomitk@users.noreply.github.com>
2019-03-05 12:07:18 +0900

    add crawling function

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
+const crawler = require('./lib/crawler')
+
 exports.handler = async (event, context, callback) => {
   console.log('Received event:', JSON.stringify(event, null, 2))
-  // do something
+
+  const crawlTargetUrl = 'http://example.com'
+  await crawler.crawl(crawlTargetUrl)
+  
   callback(null)
 }
